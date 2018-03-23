11ddd489aa6cdf2cfb11ee53fa162e5df547bb4a
Glauber Carvalho <glaubercarv@gmail.com>
2018-03-23 10:42:57 -0300

    4.3
    Better organization of files in folders;  It was written comments in all files;  Maude files added on data directory;

--- callMaude.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#DIR_FILE_MAUDE="/home/glauberca/Documentos/Silver/semantics.maude"
-#DIR_MAUDE="/usr/bin/maude"
-#command="red generateDot({$choice}, $input) ."
-COMMAND=$1
-echo $COMMAND
-
-#Result $COMMAND | $DIR_MAUDE $DIR_FILE_MAUDE -no-banner
